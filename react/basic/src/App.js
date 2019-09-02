fb230abfe439bd42021fcbf222809f491649f871
galihi5 <www.galih@gmail.com>
2019-09-02 22:24:41 +0700

    tambah react

---
+++ react/basic/src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: 1, name: 'Galih', age: 29 },
+      { id: 2, name: 'Aditya', age: 30 },
+      { id: 3, name: 'Wicaksono', age: 31 }
+    ]
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({
+      persons: persons
+    });
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons })
+  }
+
+  render() {
+    let person = (
+      <div>
+        {this.state.persons.map((person, index) => {
+          return <Person
+            key={person.id}
+            name={person.name}
+            age={person.age}
+            click={() => this.deletePersonHandler(index)}
+            changed={(event) => this.nameChangedHandler(event, person.id)}
+          />
+        })}
+      </div>
+    );
+
+    return (
+      <div className="App">
+        {person}
+      </div>
+    );
+  }
+}
+
+export default App;
